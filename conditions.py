ea9a757a60674b70d88add298856dec4e13f61df
vinothkumarnair <vinothkumarnair@gmail.com>
2020-12-21 13:17:05 +0530

    Create conditions.py

---
+++ conditions.py
@@ -0,0 +1,25 @@
+
+
+#simple conditions
+
+user_name = input("enter your name")
+
+if user_name == "vinoth" :
+  print("its me")
+elif user_name == "kumar" :
+  print("its me again")
+else:
+  print("others")
+
+print("done compare")
+
+#compare with list
+
+my_name_list = ["vinoth","kumar","nair"]
+
+name_to_check = input("enter your name")
+
+if name_to_check in my_name_list :
+  print("name found")
+else:
+  print("not found")
