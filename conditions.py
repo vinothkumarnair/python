ec7011540524df555c4d6f88f17652ae97a63887
vinothkumarnair <vinothkumarnair@gmail.com>
2020-12-22 12:24:40 +0530

    Update conditions.py

--- conditions.py
+++ conditions.py
@@ -23,3 +23,7 @@ if name_to_check in my_name_list :
   print("name found")
 else:
   print("not found")
+  
+# to check whether instance is of type
+#if not isinstance(car,Car):
+  #raise TypeError("something")
